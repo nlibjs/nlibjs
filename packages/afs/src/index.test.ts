b32c9c1567b10c7deb96945c046e7e5712d4905f
Kei Ito <kei.itof@gmail.com>
2018-12-20 00:34:51 +0900

    chore: change ava configuration

--- packages/afs/src/index.test.ts
+++ packages/afs/src/index.test.ts
@@ -3,3 +3,11 @@ import test from 'ava';
 test('my passing test', (t) => {
     t.pass();
 });
+
+test('my passing async test', async (t) => {
+    await 0;
+    await new Promise((resolve) => {
+        setTimeout(resolve, 100);
+    });
+    t.pass();
+});
