d3cd49ea2eb4563466f6afe95d972b8356d06577
Kei Ito <kei.itof@gmail.com>
2018-12-20 14:56:46 +0900

    refactor(afs): refactor absolutify.ts

--- packages/afs/src/absolutify.ts
+++ packages/afs/src/absolutify.ts
@@ -1,7 +1,14 @@
 import {PathLike} from 'fs';
 import {isAbsolute, normalize, join} from 'path';
 
-export const absolutify = (file: PathLike, base = process.cwd()): string => {
-    const stringified = `${file}`;
-    return isAbsolute(stringified) ? normalize(stringified) : join(base, stringified);
+export const absolutify = (
+    filePath: PathLike,
+    baseDirectory = process.cwd(),
+): string => {
+    const stringified = `${filePath}`;
+    if (isAbsolute(stringified)) {
+        return normalize(stringified);
+    } else {
+        return join(baseDirectory, stringified);
+    }
 };
