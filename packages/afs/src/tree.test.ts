9dd8bfe9562c3d7cd0a6a6ba9ffd4eb90540ad5f
Kei Ito <kei.itof@gmail.com>
2018-12-25 16:54:26 +0900

    test(afs): add tests for tree

---
+++ packages/afs/src/tree.test.ts
@@ -0,0 +1,60 @@
+import {tmpdir} from 'os';
+import {join} from 'path';
+import anyTest, {TestInterface} from 'ava';
+import {
+    mkdtemp,
+    rmrf,
+    tree,
+    mkdir,
+    writeFile,
+    symlink,
+} from '.';
+
+const test = anyTest as TestInterface<{
+    directory: string
+}>;
+
+test.beforeEach(async (t) => {
+    t.context.directory = await mkdtemp(tmpdir());
+});
+
+test.afterEach(async (t) => {
+    await rmrf(t.context.directory);
+});
+
+test('return a tree', async (t) => {
+    const dir1 = join(t.context.directory, 'foo');
+    await mkdir(dir1);
+    const file1 = join(dir1, 'bar');
+    await writeFile(file1, file1);
+    const result = await tree(t.context.directory);
+    t.is(result.type, 'directory');
+    t.is(result.path, t.context.directory);
+    t.truthy(result.files.foo);
+    t.is(result.files.foo.type, 'directory');
+    t.is(result.files.foo.path, dir1);
+    t.truthy(result.files.foo.files.bar);
+    t.is(result.files.foo.files.bar.type, 'file');
+    t.is(result.files.foo.files.bar.path, file1);
+});
+
+test('return a tree with symbolic link', async (t) => {
+    const dir1 = join(t.context.directory, 'foo');
+    await mkdir(dir1);
+    const file1 = join(dir1, 'bar');
+    await writeFile(file1, file1);
+    const symlink1 = join(dir1, 'baz');
+    await symlink(file1, symlink1);
+    const result = await tree(t.context.directory);
+    t.is(result.type, 'directory');
+    t.is(result.path, t.context.directory);
+    t.truthy(result.files.foo);
+    t.is(result.files.foo.type, 'directory');
+    t.is(result.files.foo.path, dir1);
+    t.truthy(result.files.foo.files.bar);
+    t.is(result.files.foo.files.bar.type, 'file');
+    t.is(result.files.foo.files.bar.path, file1);
+    t.truthy(result.files.foo.files.baz);
+    t.is(result.files.foo.files.baz.type, 'symboliclink');
+    t.is(result.files.foo.files.baz.path, symlink1);
+});
