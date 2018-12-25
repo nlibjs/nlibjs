cafde120771bec027a7a9f8d4461287f18bc4a4e
Kei Ito <kei.itof@gmail.com>
2018-12-25 14:22:11 +0900

    test(afs): add tests for mkdirp

---
+++ packages/afs/src/mkdirp.test.ts
@@ -0,0 +1,37 @@
+import {tmpdir} from 'os';
+import {join} from 'path';
+import anyTest, {TestInterface} from 'ava';
+import {
+    mkdtemp,
+    stat,
+    writeFile,
+} from './core';
+import {rmrf} from './rmrf';
+import {mkdirp} from './mkdirp';
+
+const test = anyTest as TestInterface<{
+    directory: string
+}>;
+
+test.beforeEach(async (t) => {
+    t.context.directory = await mkdtemp(join(tmpdir(), 'cpr'));
+});
+
+test.afterEach(async (t) => {
+    await rmrf(t.context.directory);
+});
+
+test('create a directory', async (t) => {
+    const dirPath = join(t.context.directory, 'dir1', 'dir2', 'dir3');
+    await mkdirp(dirPath);
+    t.true((await stat(dirPath)).isDirectory());
+});
+
+test('throw an error if there is a file', async (t) => {
+    const dirPath1 = join(t.context.directory, 'dir1', 'dir2', 'dir3');
+    await mkdirp(dirPath1);
+    t.true((await stat(dirPath1)).isDirectory());
+    const dirPath2 = join(dirPath1, 'dir4');
+    await writeFile(dirPath2, dirPath2);
+    await t.throwsAsync(() => mkdirp(dirPath2));
+});
