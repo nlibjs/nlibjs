faad8246f8bbf395778a100602ba3f7b57106823
Kei Ito <kei.itof@gmail.com>
2018-12-26 11:46:20 +0900

    refactor(afs): remove fallback

--- packages/afs/src/getHash.ts
+++ packages/afs/src/getHash.ts
@@ -9,7 +9,7 @@ export const getHash = (
     data: Parameters<Hash['update']>[0],
     {algorithm, encoding}: HashOptions = {algorithm: 'sha256', encoding: 'latin1'},
 ): string | Buffer => {
-    const hash = createHash(algorithm || 'sha256');
+    const hash = createHash(algorithm);
     hash.update(data);
     return encoding === Buffer ? hash.digest() : hash.digest(encoding as HexBase64Latin1Encoding);
 };
