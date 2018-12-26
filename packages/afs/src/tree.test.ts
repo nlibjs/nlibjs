225c9d60abf27eff5f1100cd0da455ee4984dc51
Kei Ito <kei-ito@users.noreply.github.com>
2018-12-26 14:32:35 +0900

    test(afs): test fifo (#5)
    * test(afs): test fifo
    
    * fix(afs): check fifo.lnk
    
    * fix(afs): skip test if mkfifo is not supported

--- packages/afs/src/tree.test.ts
+++ packages/afs/src/tree.test.ts
@@ -8,6 +8,7 @@ import {
 import {mktempdir} from './mktempdir';
 import {tree} from './tree';
 import * as index from '.';
+import {exec} from 'child_process';
 
 const test = anyTest as TestInterface<{
     directory: string
@@ -56,4 +57,29 @@ test('return a tree with symbolic link', async (t) => {
     t.truthy(result.files.foo.files.baz);
     t.is(result.files.foo.files.baz.type, 'symboliclink');
     t.is(result.files.foo.files.baz.path, symlink1);
+});
+
+test('return a tree with fifo', async (t) => {
+    const dir = join(t.context.directory, 'foo');
+    await mkdir(dir);
+    const fifoPath = join(dir, 'fifo');
+    await new Promise((resolve, reject) => {
+        exec(`mkfifo ${fifoPath}`, (error, stdout, stdin) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve({stdin, stdout});
+            }
+        });
+    });
+    const result = await tree(t.context.directory);
+    t.is(result.type, 'directory');
+    t.is(result.path, t.context.directory);
+    t.truthy(result.files.foo);
+    t.is(result.files.foo.type, 'directory');
+    t.is(result.files.foo.path, dir);
+    if (result.files.foo.files.fifo) {
+        t.is(result.files.foo.files.fifo.type, 'fifo');
+        t.is(result.files.foo.files.fifo.path, fifoPath);
+    }
 });
